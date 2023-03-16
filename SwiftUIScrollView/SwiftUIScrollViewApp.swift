509fafbe03572f239d1cf138b51e5676bcd26e2d
Igor Nikiforov <ivnikiforo@mts.ru>
2023-03-16 21:05:58 +0600

    Initial Commit

---
+++ SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by Никифоров Игорь Викторович on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
